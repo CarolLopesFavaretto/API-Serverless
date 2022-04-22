53a9fdecf82261c2b035633929838d2d3e5451f5
CarolLopesFavaretto <carolinelopesrdg@hotmail.com>
2022-04-22 00:32:18 -0300

    integration with dynamonDB

--- handler.js
+++ handler.js
@@ -6,18 +6,33 @@ const pacientes = [
   { id: 3, nome: "Jose", dataNascimento: "1998-06-06" },
 ];
 
-module.exports.listarPacientes = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        pacientes,
-      },
-      null,
-      2
-    ),
-  };
+const AWS = require('aws-sdk');
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const params = {
+  TableName: "PACIENTES",
 };
+
+module.exports.listarPacientes = async event => {
+  try {
+    let data = await dynamoDb.scan(params).promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data.Items),
+    };
+  } catch (err) {
+    console.log("Error", err);
+    return {
+      statusCode: err.statusCode ? err.statusCode : 500,
+      body: JSON.stringify({
+        error: err.name ? err.name : "Exception",
+        message: err.message ? err.message : "Unknown error",
+      }),
+    };
+  }
+};
+
 module.exports.obterPaciente = async event => {
 
   const { pacienteId } = event.pathParameters;
