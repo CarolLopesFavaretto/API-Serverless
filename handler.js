328b55b8a3e05c84b204130a8c3a90561ac79102
CarolLopesFavaretto <carolinelopesrdg@hotmail.com>
2022-04-21 23:51:09 -0300

    created endpoints

--- handler.js
+++ handler.js
@@ -1,24 +1,37 @@
-'use strict';
+"use strict";
 
 const pacientes = [
-  { id: 1, nome: 'Maria', dataNascimento: '1984-11-01' },
-  { id: 2, nome: 'Joao', dataNascimento: '1980-01-16' },
-  { id: 3, nome: 'Jose', dataNascimento: '1998-06-06' }
-]
+  { id: 1, nome: "Maria", dataNascimento: "1984-11-01" },
+  { id: 2, nome: "Joao", dataNascimento: "1980-01-16" },
+  { id: 3, nome: "Jose", dataNascimento: "1998-06-06" },
+];
 
 module.exports.listarPacientes = async event => {
-  console.log(event)
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        pacientes
+        pacientes,
       },
       null,
       2
-    )
+    ),
+  };
+};
+module.exports.obterPaciente = async event => {
+
+  const { pacienteId } = event.pathParameters;
+  const paciente = pacientes.find((paciente) => paciente.id == pacienteId);
+
+  if(paciente === undefined){
+    return {
+    statusCode: 404,
+    body: JSON.stringify({ error:"Paciente não existe"}, null, 2),
+    }
   }
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-}
+  return {
+    statusCode: 200,
+    body: JSON.stringify(paciente, null, 2),
+  };
+};
